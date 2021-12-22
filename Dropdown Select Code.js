bee5e16884f8a507de8354568d2c03dffda0be33
Ben Swift <ben@percolatorconsulting.com>
2021-12-22 09:44:51 -0500

    12/22/ Updates
    Modified to use template literals instead of setting field ids multiple times. Added functionality
    to pass a different value from the repeater than the label displayed in the dropdown

--- Dropdown Select Code.js
+++ Dropdown Select Code.js
@@ -25,94 +25,130 @@ The example below, on Windows Load and Select change,
 2. Uses the connector to grab the Repeater section of selected fields based on a list in a repeating section
 3. Loop through the Repeaters and get the REPLACEVALUESFIELD values and put them in an array
 4. Add the Array values into the dropdown (TARGETFIELD)
+
+Date Modified: 12/22/2021
+Modified Notes: Modified by Ben Swift to use template literals instead of setting field ids multiple times. Added functionality
+to pass a different value from the repeater than the label displayed in the dropdown
+
+Set field Ids
 */
  </script>
 <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
 
 <script>
-// ##########
-// #FORM LOAD
-// ##########
- 
- 
-	  	// On Load for the page
-$(window).load(function() {
-      //alert("window load occurred!");
-	//Clear dropdown options
-	document.getElementById("TARGETFIELD").options.length=0;
-	// Add the select option message (Can be anything you want wrapped in double quotes)
-	var option = document.createElement("option");
-	option.text = "This element has been taken over!!!!! ...";
-	document.getElementById("TARGETFIELD").add(option);//Dropdown field
- 
-  
- //Loop through repeater and get all the values for field and Add them to an Array
-			
-	  		// Gets all the fields from the form, including the hidden fields
-			//Dropdown fields
-	  		var dropdownAccount = document.getElementById("TARGETFIELD");//Dropdown field to be updated
-			
-			// Text Repeater field
-			var eventDisplay4Arr = [];// Array for storing all the dropdown names 
-			var eventDisplay4Arr = document.querySelectorAll('[id^=REPLACEVALUESFIELD]'); // Example: all the account names from repeating section
-				  
-			var textToWrite; // temp variable for writing to arrays
 
-			//For all contents of the eventDisplay4 array (from repeater - REPLACEVALUESFIELD) ..
-          for(var i in eventDisplay4Arr){
+	//####SET FIELD IDS HERE####
+	//dropdown field
+	let theDropDown = 'tfa_4';//SET THE ID FOR THE DROPDOWN FIELD
+	//repeater label
+	let theLabel = 'tfa_11';//SET THE ID FOR THE FIELD IN THE REPEATER CONTAINING THE LABEL TO DISPLAY IN THE DROPDOWN (I.E. ACCOUNT NAME)
+	//repeater value
+	let theValue = 'tfa_12';//SET THE ID FOR THE FIELD IN THE REPEATER CONTAINING THE VALUE TO PASS IN TO SALESFORCE (I.E. ACCOUNT ID)
+	//field to hold the chosen value
+	let theChoice = 'tfa_8';//SET THE ID FOR THE FIELD TO HOLD THE CHOSEN VALUE AND TO PASS INTO SALESFORCE
+
+
+	// ##########
+	// #FORM LOAD
+	// ##########
+
+	// On Load for the page
+	$(window).load(function() {
+		//alert("window load occurred!");
+		//Clear dropdown options
+		document.getElementById(theDropDown).options.length=0;
+		// Add the select option message (Can be anything you want wrapped in double quotes)
+		var option = document.createElement("option");
+		option.text = "This element has been taken over!!!!! ...";
+		document.getElementById(theDropDown).add(option);//Dropdown field
+	 
+	  
+		//Loop through repeater and get all the values for field and Add them to an Array
 				
-				//assign current iterations value to var.
-		   		textToWrite = eventDisplay4Arr[i].value;
+		// Gets all the fields from the form, including the hidden fields
+		//Dropdown fields
+		var dropdownAccount = document.getElementById(theDropDown);//Dropdown field to be updated
 				
-				//if it contains a value ...
-				if (textToWrite) {
-					//add the eventDisplay4 value to the selection 'main' dropdown
-					addToDropdown(textToWrite); //This adds the text to the dropdown box ...
-																		
-					if (textToWrite == "" || textToWrite == null) {
-						
-						alert("EventDisplay4 TextToWrite BLANK - please contact administrator.");
-		   				textToWrite = "No eventDisplay4 Available."; // Stores the value in the array
-	   				}
-                  eventDisplay4Arr[i] = textToWrite; //assign textToWrite value to corresponding index of EventDisplay4 array
-													 // if the contents were blank, it would push 'No eventDisplay4 Available.'
+		// Text Repeater field
+		var eventDisplay4Arr = [];// Array for storing all the dropdown names 
+		var eventDisplay4Arr = document.querySelectorAll(`[id^=${theLabel}]`); // Example: all the account names from repeating section
+					  
+		var textToWrite; // temp variable for writing to arrays
+
+		//For all contents of the eventDisplay4 array (from repeater - theLabel) ..
+		for(var i in eventDisplay4Arr){
+					
+			//assign current iterations value to var.
+			textToWrite = eventDisplay4Arr[i].value;
+					
+			//if it contains a value ...
+			if (textToWrite) {
+				//add the eventDisplay4 value to the selection 'main' dropdown
+				addToDropdown(textToWrite); //This adds the text to the dropdown box ...
+																			
+				if (textToWrite == "" || textToWrite == null) {
+					alert("EventDisplay4 TextToWrite BLANK - please contact administrator.");
+					textToWrite = "No eventDisplay4 Available."; // Stores the value in the array
 				}
-			}  //end of eventDisplay4 for loop
-   
-   /////////////////////////////////////////////////////////////////////////////////////
-   /////////////////////////////////////////////////////////////////////////////////////
-  //----------------------------------
-  
-  	// Function to add to the dropdown
+				eventDisplay4Arr[i] = textToWrite; //assign textToWrite value to corresponding index of EventDisplay4 array
+				// if the contents were blank, it would push 'No eventDisplay4 Available.'
+			}
+		}  //end of eventDisplay4 for loop
+	   
+	   /////////////////////////////////////////////////////////////////////////////////////
+	   /////////////////////////////////////////////////////////////////////////////////////
+	  //----------------------------------
+	  
+		// Function to add to the dropdown
 		function addToDropdown(text) {
-		    var x = document.getElementById("TARGETFIELD");//Dropdown field you are replacing values in
+			var x = document.getElementById(theDropDown);//Dropdown field you are replacing values in
 			var option = document.createElement("option");
 			option.text = text;
 			x.add(option);
 		}
-  
- 	 //-------------------------------------
 	  
-	  //Add the selected dropdown value into fields on select change
-	  $(document).ready(function() {
+		//-------------------------------------
+		  
+		//Add the selected dropdown value into fields on select change
+		$(document).ready(function() {
 
-	  //the id here should be for the FIELD on the form
-	  //this line is listening for a change to the field in order to execute the code
-	  $("#REPLACEVALUESFIELD").change(function(){
+			//the id here should be for the FIELD on the form
+			//this line is listening for a change to the field in order to execute the code
+			$(`#${theLabel}`).change(function(){
+			
+				var index = $(`#${theLabel} option:selected`).index()-1;
+				//alert('Selected Index is: ' + index);
+
+				var indexLength =$(`#${theLabel} option`).length;
+
 		
-	var index = $('#REPLACEVALUESFIELD option:selected').index()-1;
-	//alert('Selected Index is: ' + index);
+				var CountIndex = $(`#${theDropDown}`)[0].selectedIndex;
+			
+	   
+			});//Close tfa_XX-Change-function	
+			
+			return;
+		});//Close Document-ready-function  
 
-	var indexLength =$('#REPLACEVALUESFIELD option').length;
+		//add event listener to dropdown to get chosen value from label
+		document.getElementById(theDropDown).addEventListener('change',setChoice);  
 
-	
-	var CountIndex = $("#REPLACEVALUESFIELD")[0].selectedIndex;
+	});//Close- Load windows Function
+
+	//function to grab corresponding value from chosen label and set in choice field
+	function setChoice(){
+		let theDropDownLabel = document.getElementById(theDropDown).value;//the current selection in the dropdown
+		let theChoiceField = document.getElementById(theChoice);//the field to pass the value from the repeater and send to Salesforce
 		
-   
-	  });//Close tfa_XX-Change-function
-	return;
-	});//Close Document-ready-function  
-});//Close- Load windows Function
+		let labelArray = document.querySelectorAll(`[id^=${theLabel}]`); //all of the labels from the repeater
+		let valueArray = document.querySelectorAll(`[id^=${theValue}]`); //all of the values from the repeater
+		
+		//cycle through each label in repeater
+		for(let i in theDropDownLabel){
+			if((labelArray[i]) && labelArray[i].value == theDropDownLabel){//if the current label matches the choice in the dropdown
+				theChoiceField.value = valueArray[i].value;//send the corresponding value from the repeater to the choice field			
+			}
+		}
+	}
 
 </script>
-
